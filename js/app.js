99976ef6fde2280bdf089a362643985bee2f7a38
Akash Hasendra <akashhasendra2@gmail.com>
2023-03-19 02:21:52 +0530

    V 0.4
    Ui improvement & Add question function

--- js/app.js
+++ js/app.js
@@ -1,66 +1,149 @@
-// define quiz questions with their respective choices and answers
-const quizData = [
+$(document).ready(function () {
+  var questions = [
     {
       question: "What is the capital of France?",
-      choices: ["London", "Paris", "Berlin", "Madrid"],
-      answer: "Paris"
+      choices: ["Paris", "Rome", "Madrid", "Berlin"],
+      correctAnswer: 0,
     },
     {
-      question: "What is the largest mammal in the world?",
-      choices: ["Elephant", "Giraffe", "Whale", "Hippopotamus"],
-      answer: "Whale"
+      question: "What is the largest country in the world?",
+      choices: ["China", "USA", "Russia", "Australia"],
+      correctAnswer: 2,
     },
     {
-      question: "What is the smallest country in the world?",
-      choices: ["Monaco", "Vatican City", "San Marino", "Liechtenstein"],
-      answer: "Vatican City"
-    }
+      question: "What is the highest mountain in the world?",
+      choices: ["Mount Everest", "Kilimanjaro", "Mount Fuji", "Matterhorn"],
+      correctAnswer: 0,
+    },
   ];
-  
-  let currentQuestion = 0;
-  let score = 0;
-  
-  // display the current question and its choices
-  function showQuestion() {
-    const questionEl = $("#question");
-    const choicesEl = $("#choices");
-  
-    questionEl.text(quizData[currentQuestion].question);
-    choicesEl.empty();
-  
-    quizData[currentQuestion].choices.forEach((choice) => {
-      const li = $("<li></li>");
-      li.text(choice);
-      li.click(() => checkAnswer(choice));
-      choicesEl.append(li);
-    });
-  }
-  
-  // check if user's answer is correct and update score accordingly
-  function checkAnswer(answer) {
-    if (answer === quizData[currentQuestion].answer) {
-      score++;
+
+  var currentQuestion = 0;
+  var score = 0;
+  var totalQuestions = questions.length;
+
+  // Display the current question and choices
+  function displayQuestion() {
+    var question = questions[currentQuestion];
+    $("#question").text(question.question);
+    $("#choices").empty();
+    for (var i = 0; i < question.choices.length; i++) {
+      $("#choices").append(
+        "<li><input type='radio' name='choice' value='" +
+          i +
+          "' id='choice" +
+          i +
+          "'><label for='choice" +
+          i +
+          "'>" +
+          question.choices[i] +
+          "</label></li>"
+      );
     }
-    currentQuestion++;
-  
-    // move to next question or end the quiz if all questions have been answered
-    if (currentQuestion < quizData.length) {
-      showQuestion();
+  }
+
+  // Check the user's answer and update the score
+  function checkAnswer() {
+    var selectedChoice = $("input[name='choice']:checked").val();
+    if (selectedChoice == undefined) {
+      alert("Please select an answer.");
     } else {
-      const quizContainer = $("#quiz-container");
-      quizContainer.html(`<h2>You scored ${score} out of ${quizData.length}</h2>`);
+      if (selectedChoice == questions[currentQuestion].correctAnswer) {
+        score++;
+      }
+      currentQuestion++;
+      if (currentQuestion < totalQuestions) {
+        displayQuestion();
+      } else {
+        displayScore();
+        $("#submit-btn").hide();
+        $("#reset-btn").show();
+      }
     }
   }
-  
-  // start the quiz by showing the first question
-  showQuestion();
-  
-  // add event listener to submit button
-  const submitBtn = $("#submit-btn");
-  submitBtn.click(() => checkAnswer());
-  
-  // highlight selected choice on click
-  $("#choices li").click(function() {
-    $(this).addClass("selected").siblings().removeClass("selected");
+
+  // Display the user's score
+  function displayScore() {
+    $("#question").text("Quiz Complete!");
+    $("#choices").empty();
+    $("#score").text(score + " out of " + totalQuestions);
+  }
+
+  // Reset the quiz
+  function resetQuiz() {
+    currentQuestion = 0;
+    score = 0;
+    displayQuestion();
+    $("#score").text(score);
+    $("#submit-btn").show();
+    $("#reset-btn").hide();
+  }
+
+  // Function to add a new question to the list
+  function addNewQuestion() {
+    // Get the values of the new question and answer inputs
+    var newQuestion = $("#new-question-input").val();
+    var newAnswer = $("#new-answer-input").val();
+
+    // Create a new list item with the new question and answer
+    var newListItem = $("<li>").html(
+      "<strong>" + newQuestion + "</strong><br>" + newAnswer
+    );
+
+    // Add the new list item to the question list
+    $("#question-list").append(newListItem);
+
+    // Clear the input fields
+    $("#new-question-input").val("");
+    $("#new-answer-input").val("");
+  }
+
+  // Bind the addNewQuestion function to the Add Question button
+  $("#add-question-btn").click(function () {
+    addNewQuestion();
   });
-  
+
+  function addQuestion() {
+    var questionInput = $("#new-question-input").val();
+    var answerInput = $("#new-answer-input").val();
+
+    // Check that the input fields are not empty
+    if (questionInput && answerInput) {
+      // Create a new question object with user input values
+      var newQuestion = {
+        question: questionInput,
+        answer: answerInput,
+      };
+
+      // Add the new question to the questions array
+      questions.push(newQuestion);
+
+      // Append a new list item to the question list with the new question text
+      var newQuestionItem = $("<li>").text(newQuestion.question);
+      $("#question-list").append(newQuestionItem);
+
+      // Clear the input fields
+      $("#new-question-input").val("");
+      $("#new-answer-input").val("");
+    }
+  }
+
+  $("#add-question-btn").click(function () {
+    var newQuestion = $("#new-question-input").val();
+    var newAnswer = $("#new-answer-input").val();
+
+    // Do something with the new question and answer data
+  });
+
+  // Event handlers
+  $("#submit-btn").click(function () {
+    checkAnswer();
+  });
+
+  $("#reset-btn").click(function () {
+    resetQuiz();
+  });
+
+  // Initial setup
+  displayQuestion();
+  $("#reset-btn").hide();
+});
